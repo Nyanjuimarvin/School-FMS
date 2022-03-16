de4dc8dfe42e651b8498d597c2203a62a1ed13f1
marvin <marnyanjui@gmail.com>
2022-03-16 13:44:28 +0300

    Add school class tests

---
+++ src/test/java/Models/SchoolTest.java
@@ -0,0 +1,33 @@
+package Models;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SchoolTest {
+
+    @Test
+    @DisplayName("Instantiates Correctly")
+    public void School_InstantiatesCorrectly(){
+        School school = setUpSchool();
+        assertTrue(school instanceof  School);
+    }
+
+    @Test
+    @DisplayName("Properties are passed")
+    public void School_HasProperties(){
+        School school = setUpSchool();
+        assertEquals("Combine High",school.getName());
+        assertEquals("Division 17",school.getRegion());
+    }
+
+
+    public School setUpSchool(){
+        School school = School.getInstance();
+        school.setName("Combine High");
+        school.setRegion("Division 17");
+        return school;
+    }
+
+}
