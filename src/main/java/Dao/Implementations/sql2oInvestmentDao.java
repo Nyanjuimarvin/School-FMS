343a64fba8d73af1a8e75d4bbcec15ba5f6f2c71
stocksociety <arnold.kagunda@student.moringaschool.com>
2022-03-17 18:32:36 +0300

    add investment class

--- src/main/java/Dao/Implementations/sql2oInvestmentDao.java
+++ src/main/java/Dao/Implementations/sql2oInvestmentDao.java
@@ -2,10 +2,29 @@ package Dao.Implementations;
 
 import Dao.Interfaces.InvestmentDao;
 import Models.Investment;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 public class sql2oInvestmentDao implements InvestmentDao {
+    private final Sql2o sql2o;
+
+    public sql2oInvestmentDao(Sql2o sql2o){this.sql2o=sql2o;}
     @Override
     public void add(Investment investment) {
 
+        String insert = "INSERT INTO investment ( investment,cash ) VALUES (:investment, :cash,)";
+
+
+        try(Connection con = sql2o.open()){
+            int id;
+            id = (int) con.createQuery(insert, true)
+                    .bind(investment)
+                    .executeUpdate()
+                    .getKey();
+            investment.setId(id);
+        }catch (Sql2oException error){
+            System.out.println("Object was not possible to add because of " + error);
+        }
     }
 }
--- src/main/java/Models/Investment.java
+++ src/main/java/Models/Investment.java
@@ -1,4 +1,7 @@
 package Models;
 
 public class Investment {
+    public void setId(int id) {
+    }
 }
+
